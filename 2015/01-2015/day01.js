a7ca6f9afb2eb17e42f0e55545e9eb2438185278
Daniel Carvalho <carvalho.rochadan@gmail.com>
2020-03-11 01:20:40 -0300

    Compiled ts file

--- 2015/01-2015/day01.js
+++ 2015/01-2015/day01.js
@@ -1,1 +1,41 @@
-console.log('Hello World');
+var fs = require('fs');
+var day1pt1 = function () {
+    fs.readFile('./day01_input.txt', function (err, data) {
+        console.time('time');
+        var directions = data.toString();
+        var directionsArray = directions.split('');
+        var answer = directionsArray.reduce(function (accumulator, currentValue) {
+            if (currentValue === '(') {
+                return accumulator += 1;
+            }
+            else {
+                return accumulator -= 1;
+            }
+        }, 0);
+        console.timeEnd('time');
+        console.log(answer);
+    });
+};
+day1pt1();
+var day1pt2 = function () {
+    fs.readFile('./day01_input.txt', function (err, data) {
+        console.time('time-position');
+        var directions = data.toString();
+        var directionsArray = directions.split('');
+        var accumulator = 0;
+        var counter = 0;
+        var answer = directionsArray.some(function (currentItem) {
+            if (currentItem === '(') {
+                accumulator += 1;
+            }
+            else {
+                accumulator -= 1;
+            }
+            counter++;
+            return accumulator < 0;
+        });
+        console.log(counter);
+        console.timeEnd('time-position');
+    });
+};
+day1pt2();
