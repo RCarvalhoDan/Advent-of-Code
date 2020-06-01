e8415226b52ab7d7517a5b1a0b3326e65621d7fa
Daniel Carvalho <carvalho.rochadan@gmail.com>
2020-06-01 20:44:26 -0300

    Add coordinate interface, isUnique function, and logic to move through the Cartesian Plane. Add the problem's second part to README. Solve problem first part.

--- 2015/03-2015/day03.js
+++ 2015/03-2015/day03.js
@@ -1,19 +1,46 @@
 "use strict";
 exports.__esModule = true;
 var fs = require("fs");
+// Tests if the coordinate already exists
+var isUnique = function (visitedArr, obj) {
+    return visitedArr.some(function (value) {
+        if (value.x === obj.x && value.y === obj.y)
+            return true;
+    });
+};
 var day03 = function () {
-    fs.readFile('day03_input.txt', 'utf8', function (err, data) {
+    fs.readFile("day03_input.txt", "utf8", function (err, data) {
         var directions = data.toString();
-        var directionsArr = directions.split('');
-        //TODO: Review typescript object typing. Maybe implement a interface first
-        var coordinates = { x: 0, y: 0 };
+        var directionsArr = directions.split("");
+        var cX = 0;
+        var cY = 0;
+        var position = { x: cX, y: cY };
+        var housesVisited = [];
+        // Add the first house
+        housesVisited.push(position);
         for (var i = 0; i < directionsArr.length; i++) {
-            //TODO: implement logic
-            //plot the points as in a Cartesian Plane
-            //every point should be added to a Hash Table
-            //  if the point is already in the Table, do nothing
-            //return the number of points in the Table
+            // Move in the Cartesian Plane
+            if (directionsArr[i] === "<") {
+                cX = cX - 1;
+            }
+            else if (directionsArr[i] === "^") {
+                cY = cY + 1;
+            }
+            else if (directionsArr[i] === ">") {
+                cX = cX + 1;
+            }
+            else if (directionsArr[i] === "v") {
+                cY = cY - 1;
+            }
+            // Update position value
+            position = { x: cX, y: cY };
+            // Add only new houses
+            if (!isUnique(housesVisited, position)) {
+                housesVisited.push(position);
+            }
         }
+        // Return the number of unique houses visited
+        console.log(housesVisited.length);
     });
 };
 day03();
--- 2015/03-2015/day03.ts
+++ 2015/03-2015/day03.ts
@@ -1,21 +1,60 @@
-import * as fs from 'fs';
+import * as fs from "fs";
+
+// Coordinate Type
+interface coordinate {
+  x: number;
+  y: number;
+}
+
+// Tests if the coordinate already exists
+const isUnique = (visitedArr: coordinate[], obj: coordinate): boolean =>
+  visitedArr.some((value: coordinate) => {
+    if (value.x === obj.x && value.y === obj.y) return true;
+  });
 
 const day03 = (): void => {
-  fs.readFile('day03_input.txt', 'utf8', (err: NodeJS.ErrnoException, data: Buffer) => {
-    const directions = data.toString();
-    const directionsArr: string[] = directions.split('');
+  fs.readFile(
+    "day03_input.txt",
+    "utf8",
+    (err: NodeJS.ErrnoException, data: Buffer) => {
+      const directions = data.toString();
+      const directionsArr: string[] = directions.split("");
 
-    //TODO: Review typescript object typing. Maybe implement a interface first
-    const coordinates: object = { x: 0, y: 0}
+      let cX = 0;
+      let cY = 0;
 
-    for(let i = 0; i < directionsArr.length; i++){
-      //TODO: implement logic
-      //plot the points as in a Cartesian Plane
-      //every point should be added to a Hash Table
-      //  if the point is already in the Table, do nothing
-      //return the number of points in the Table
+      let position: coordinate = { x: cX, y: cY };
+
+      const housesVisited: coordinate[] = [];
+
+      // Add the first house
+      housesVisited.push(position);
+
+      for (let i = 0; i < directionsArr.length; i++) {
+        // Move in the Cartesian Plane
+        if (directionsArr[i] === "<") {
+          cX = cX - 1;
+        } else if (directionsArr[i] === "^") {
+          cY = cY + 1;
+        } else if (directionsArr[i] === ">") {
+          cX = cX + 1;
+        } else if (directionsArr[i] === "v") {
+          cY = cY - 1;
+        }
+
+        // Update position value
+        position = { x: cX, y: cY };
+
+        // Add only new houses
+        if (!isUnique(housesVisited, position)) {
+          housesVisited.push(position);
+        }
+      }
+
+      // Return the number of unique houses visited
+      console.log(housesVisited.length);
     }
-  })
-}
+  );
+};
 
 day03();
