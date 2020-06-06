2f6f58f84c866335cfbef0519779ff59e7ad38fa
Daniel Carvalho <carvalho.rochadan@gmail.com>
2020-06-06 20:23:35 -0300

    Complete TODOs. Solve day 06 part one. Add day two to README.

--- 2015/06-2015/day06.js
+++ 2015/06-2015/day06.js
@@ -1,16 +1,69 @@
 "use strict";
 exports.__esModule = true;
 var fs = require("fs");
+// Extract the data needed in each instruction string
+var extractInstructions = function (instruction) {
+    var extractData = instruction
+        .replace(/\bturn\s\b|\bthrough\b\s/g, "") // replace 'turn' and 'through ' with empty
+        .replace(/\s/g, ",") // replace all white spaces with commas
+        .split(",");
+    return {
+        command: extractData[0],
+        startX: Number(extractData[1]),
+        startY: Number(extractData[2]),
+        endX: Number(extractData[3]),
+        endY: Number(extractData[4])
+    };
+};
 var day06 = function () {
     fs.readFile("day06_input.txt", "utf8", function (err, data) {
-        var instructions = data.toString();
-        var instructionsArr = instructions.split("\n");
+        var dataString = data.toString();
+        var dataArr = dataString.split("\n");
         var grid = [];
-        // Create 1000x1000
-        for (var i = 0; i < 999; i++) {
+        // Create 1000x1000 grid
+        for (var i = 0; i < 1000; i++) {
             grid[i] = new Array(1000);
         }
-        console.log(grid[0].length);
+        // Initialize all lights as 0 (off)
+        for (var i = 0; i < grid.length; i++) {
+            for (var j = 0; j < grid[i].length; j++) {
+                grid[i][j] = 0;
+            }
+        }
+        // Read all entries and execute instruction
+        for (var i = 0; i < dataArr.length; i++) {
+            var instruction = extractInstructions(dataArr[i]);
+            var command = instruction.command;
+            // Switch on, off or toggle lights in the given range
+            for (var i_1 = instruction.startX; i_1 <= instruction.endX; i_1++) {
+                for (var j = instruction.startY; j <= instruction.endY; j++) {
+                    if (command === "on") {
+                        grid[i_1][j] = 1;
+                    }
+                    if (command === "off") {
+                        grid[i_1][j] = 0;
+                    }
+                    if (command === "toggle") {
+                        if (grid[i_1][j] === 0) {
+                            grid[i_1][j] = 1;
+                        }
+                        else if (grid[i_1][j] === 1) {
+                            grid[i_1][j] = 0;
+                        }
+                    }
+                }
+            }
+        }
+        // Count total of lit lights
+        var countLit = 0;
+        for (var i = 0; i < grid.length; i++) {
+            for (var j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] === 1) {
+                    countLit = countLit + 1;
+                }
+            }
+        }
+        console.log(countLit);
     });
 };
 day06();
--- 2015/06-2015/day06.ts
+++ 2015/06-2015/day06.ts
@@ -9,36 +9,19 @@ interface instructions {
 }
 
 // Extract the data needed in each instruction string
-// TODO: Test function
-const extractInstructions = (rawInstruction: string): instructions => {
-  let command: string;
-  let startX: number;
-  let startY: number;
-  let endX: number;
-  let endY: number;
+const extractInstructions = (instruction: string): instructions => {
+  const extractData: string[] = instruction
+    .replace(/\bturn\s\b|\bthrough\b\s/g, "") // replace 'turn' and 'through ' with empty
+    .replace(/\s/g, ",") // replace all white spaces with commas
+    .split(",");
 
-  const turnon: string = "turnon";
-  const turnoff: string = "turnoff";
-  const toggle: string = "toggle";
-
-  const extractData = rawInstruction.split(" ").join("");
-
-  if (extractData.includes(turnon)) {
-    command = turnon;
-  }
-
-  if (extractData.includes(turnoff)) {
-    command = turnoff;
-  }
-
-  if (extractData.includes(toggle)) {
-    command = toggle;
-  }
-
-  //TODO: loop through extract data checking isNan(character) || character === ','
-  // add the number values to its respectives coordinates
-
-  return; // return an object instructions with the extracted data
+  return {
+    command: extractData[0],
+    startX: Number(extractData[1]),
+    startY: Number(extractData[2]),
+    endX: Number(extractData[3]),
+    endY: Number(extractData[4]),
+  };
 };
 
 const day06 = (): void => {
@@ -46,15 +29,61 @@ const day06 = (): void => {
     "day06_input.txt",
     "utf8",
     (err: NodeJS.ErrnoException, data: Buffer) => {
-      const instructions: string = data.toString();
-      const instructionsArr: string[] = instructions.split("\n");
+      const dataString: string = data.toString();
+      const dataArr: string[] = dataString.split("\n");
       let grid: Array<number>[] = [];
 
       // Create 1000x1000 grid
-      for (let i = 0; i < 999; i++) {
+      for (let i = 0; i < 1000; i++) {
         grid[i] = new Array<number>(1000);
       }
-      // TODO: loop through instructionsArr and applicate each string to extractInstructions
+
+      // Initialize all lights as 0 (off)
+      for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+          grid[i][j] = 0;
+        }
+      }
+
+      // Read all entries and execute instruction
+      for (let i = 0; i < dataArr.length; i++) {
+        let instruction: instructions = extractInstructions(dataArr[i]);
+        let command: string = instruction.command;
+
+        // Switch on, off or toggle lights in the given range
+        for (let i = instruction.startX; i <= instruction.endX; i++) {
+          for (let j = instruction.startY; j <= instruction.endY; j++) {
+            if (command === "on") {
+              grid[i][j] = 1;
+            }
+
+            if (command === "off") {
+              grid[i][j] = 0;
+            }
+
+            if (command === "toggle") {
+              if (grid[i][j] === 0) {
+                grid[i][j] = 1;
+              } else if (grid[i][j] === 1) {
+                grid[i][j] = 0;
+              }
+            }
+          }
+        }
+      }
+
+      // Count total of lit lights
+      let countLit: number = 0;
+
+      for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+          if (grid[i][j] === 1) {
+            countLit = countLit + 1;
+          }
+        }
+      }
+
+      console.log(countLit);
     }
   );
 };
